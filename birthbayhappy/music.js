60120b3463c8be42e7c7dfe95cd17eaf3432d4cf
kesdcca <136968368+kesdcca@users.noreply.github.com>
2026-03-02 12:04:35 +0800

    Delete birthbayhappy directory

--- birthbayhappy/music.js
+++
@@ -1,9 +0,0 @@
-
-
-const music = new Audio('Happy Birthday To You.mp3');
-music.volume = 0.5;
-
-var c = document.getElementById('canvas');
-c.addEventListener('click', () => {
-    setTimeout(() =>{ music.play();})
-})
